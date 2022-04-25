199bf9ad354b229934b80a05c24ddda3372b994f
Ridwan-Suhel <ridwansuhel96@gmail.com>
2022-04-26 00:16:34 +0600

    first commit

---
+++ __steps.js
@@ -0,0 +1,17 @@
+/**
+ * 1. create a folder [manualy or mkdir]
+ * 2. open cmd line to that folder
+ * 3. npm init
+ * 4. npm install express
+ * 5. create index.js
+ * ------------------------------
+ * For express / node server
+ * ------------------------------
+ * 1. require express
+ * 2. create app variable
+ * 3. declare port
+ * 4. set app.get()
+ * 5. listen to port
+ * 6. index.js
+ * 7. check browser
+ * */
---
+++ index.js
@@ -0,0 +1,89 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello world changes");
+});
+
+const users = [
+  {
+    id: 1,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.biz",
+  },
+  {
+    id: 2,
+    name: "Ervin Howell",
+    username: "Antonette",
+    email: "Shanna@melissa.tv",
+  },
+  {
+    id: 3,
+    name: "Clementine Bauch",
+    username: "Samantha",
+    email: "Nathan@yesenia.net",
+  },
+  {
+    id: 4,
+    name: "Patricia Lebsack",
+    username: "Karianne",
+    email: "Julianne.OConner@kory.org",
+  },
+  {
+    id: 5,
+    name: "Chelsey Dietrich",
+    username: "Kamren",
+    email: "Lucio_Hettinger@annie.ca",
+    phone: "(254)954-1289",
+    website: "demarco.info",
+  },
+  {
+    id: 6,
+    name: "Mrs. Dennis Schulist",
+    username: "Leopoldo_Corkery",
+    email: "Karley_Dach@jasper.info",
+  },
+];
+
+app.get("/users", (req, res) => {
+  console.log("query", req.query);
+  if (req.query.name) {
+    const search = req.query.name.toLowerCase();
+    const matched = users.filter((user) =>
+      user.name.toLowerCase().includes(search)
+    );
+    res.send(matched);
+  } else {
+    res.send(users);
+  }
+});
+
+app.get("/user/:id", (req, res) => {
+  console.log(req.params);
+  const id = parseInt(req.params.id);
+  const user = users.find((u) => u.id === id);
+  res.send(user);
+});
+
+app.post("/user", (req, res) => {
+  console.log("request", req.body);
+  const user = req.body;
+  user.id = users.length + 1;
+  users.push(user);
+  res.send(user);
+});
+
+app.get("/fruits", (req, res) => {
+  res.send(["Mango", "Banana", "Jackfruit"]);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port `, port);
+});
